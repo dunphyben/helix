5eecf9fb1f0093026d8825b1e18245843ca538b3
Benjamin Dunphy <dunphy.ben@gmail.com>
2013-10-30 20:03:05 -0400

    adding new rails app. first tables for helix db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helix::Application.config.secret_token = '4005c702deae5b8ca06ac58b8fc4f9e1c8a76cffbfbb7320d9f22ca48f40687141ba27b1b5167ab436e857eddeb80c82be110ac0d71f255a479dd7428bcb5f3f'
